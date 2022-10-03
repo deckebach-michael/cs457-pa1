20ab6437eb6e02db77f15377008ec050a16e4fd7
Michael <michael.deckebach@gmail.com>
2022-10-02 19:13:57 -0700

    Add README info

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# cs457-pa1
+# CS457 PA1: DATABASE MANAGEMENT SYSTEM (DBMS)
+
+## How to run the code
+You can start the dbms by running dbms.py file in the /src/ directory. Once you have the dependencies installed (see here), run the program like so:
+
+    python src/dbms.py
+
+Additionally, the dbms has been tested using the PA1 test script. The test script can be run through the dmbs like so:
+
+    python src/dbms.py < tests/pa1_test.txt
+
+## Dependencies
+This program is built using Python 3.10.4. It uses the following standard library modules:
+- csv - for file creation/deletion/manipulation
+- re - for parsing SQL statements
+- os - for directory creation/deletion as well as file/directory existance checking
+
+## How are databases structured and managed? 
+
+## How are tables structured and managed?
+
+## Functionality implemented (at a high level)
+
+## Questions? Comments?
+Email me (Michael Deckebach) at michael.deckebach@gmail.com
--- src/dbms.py
+++ src/dbms.py
@@ -2,45 +2,11 @@
 Name: dbms.py
 Author: Michael Deckebach
 Date: 2022-10-01
-Description: A simple database management system implemented in pure python.
-Creates a command line environment for simple SQL statements.
-
-Supported statements and syntax include:
-
-    ALTER TABLE <table> ADD <field> <datatype>;
-    CREATE DATABASE <database>;
-    CREATE TABLE <table> (<field> <datatype>, <field> <datatype>, ...);
-    DROP DATABASE <database>;
-    DROP TABLE <table>;
-    SELECT * FROM <table>;
-    USE <database>;
-
-Additionally, the following commands are supported:
-
-    EXIT - terminates the program
-
-Semicolons (;) are strictly required to denote a SQL statement, but not required
-for additional, non-SQL interface commands like EXIT.
+Description: Wrapper for database management program (dbms) program
 '''
 
-from statement import StatementFactory
-from utils import get_input
+from interface import main
 
 
-def main(args=None):
-
-    while True:
-        try:
-            statements = get_input()
-            factory = StatementFactory()
-
-            for s in statements:
-                if s == 'EXIT':
-                    print('All done.')
-                    quit()
-
-                stmnt = factory.make_statement(s)
-                stmnt.execute()
-
-        except Exception as exc:
-            print(exc)
+if __name__ == '__main__':
+    main()
